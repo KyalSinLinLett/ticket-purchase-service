b11010e8494b3218f5c4fc30454eaede6870cc85
kyalsinviatick <kyal.sin@viatick.com>
2024-08-15 16:45:30 +0700

    added dependency injection container

--- src/init.ts
+++ src/init.ts
@@ -3,32 +3,15 @@ import { configuration } from "config";
 import { initModels } from "db/init";
 
 import { UserController } from "controllers/UserController";
-import { UserService } from "services/user";
-import { UserRepository } from "services/user/user.repository";
-const userRepository = new UserRepository();
-const userService = new UserService(userRepository);
-
-import { AccessTokenService } from "services/accessToken";
-import { AccessTokenRepository } from "services/accessToken/accessToken.repository";
-const accessTokenRepository = new AccessTokenRepository();
-const accessTokenService = new AccessTokenService(accessTokenRepository);
-
 import { EventController } from "controllers/EventController";
-import { EventService } from "services/event";
-import { EventRepository } from "services/event/event.repository";
-const eventRepository = new EventRepository();
-const eventService = new EventService(eventRepository);
-
 import { TicketController } from "controllers/TicketController";
-import { TicketCategoryService } from "services/ticketCategory";
-import { TicketCategoryRepository } from "services/ticketCategory/ticketCategory.repository";
-const ticketCategoryRepository = new TicketCategoryRepository();
-const ticketCategoryService = new TicketCategoryService(ticketCategoryRepository);
-
-import { TicketService } from "services/ticket";
-import { TicketRepository } from "services/ticket/ticket.repository";
-const ticketRepository = new TicketRepository();
-const ticketService = new TicketService(ticketRepository);
+import {
+  accessTokenService,
+  eventService,
+  ticketCategoryService,
+  ticketService,
+  userService
+} from "utils/DI/container";
 
 const {
   dbHost,
